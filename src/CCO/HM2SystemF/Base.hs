5609b82d853778f062bfdb2a3240b36877d3ce21
Paul van der Walt <paul@denknerd.org>
2011-04-05 21:20:38 +0100

    comments

--- src/CCO/HM2SystemF/Base.hs
+++ src/CCO/HM2SystemF/Base.hs
@@ -19,14 +19,13 @@ import CCO.Types
 import CCO.HM.Base (HMTm (..))
 import CCO.SystemF.Base (SFTm (..))
 
--- instance Printable Tm where
 runAlgoW :: HMTm -> SFTm
 ------- TODO WARNING
 -- DO Something more here, this isn't the last step!
 runAlgoW t = let (sf,ty,tysbst) = algoW_Syn_HMTm (wrap_HMTm (sem_HMTm t) inh_HMTm)
              in sf
--- 
--- -- | The top-level inherited attributes to be passed to an attribute grammar
--- -- for System F.
+-- | The top-level inherited attribute to be passed to an attribute grammar
+-- for System F. In our case, we want to start with an empty type 
+-- environment.
 inh_HMTm :: Inh_HMTm
 inh_HMTm = Inh_HMTm {typeEnvironment_Inh_HMTm = []}
