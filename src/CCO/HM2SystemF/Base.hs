d18abfc2c8e01bdd5359d06db9953d9574f47194
Paul van der Walt <paul@denknerd.org>
2011-04-06 17:34:09 +0100

    now correctly type-annotates the simplest possible program

--- src/CCO/HM2SystemF/Base.hs
+++ src/CCO/HM2SystemF/Base.hs
@@ -14,20 +14,39 @@
 
 module CCO.HM2SystemF.Base (runAlgoW) where
 
-import CCO.HM2SystemF.AG (algoW_Syn_HMTm, wrap_HMTm, sem_HMTm, Inh_HMTm (..))
+import CCO.HM2SystemF.AG (
+    algoW_Syn_HMTm, 
+    wrap_HMTm, 
+    sem_HMTm, 
+    Inh_HMTm (..),
+    gen
+    )
 import CCO.Types
 import CCO.HM.Base (HMTm (..))
-import CCO.SystemF.Base (SFTm (..))
+import CCO.SystemF.Base (SFTm (..), SFTy (..))
+import Debug.Trace
 
-runAlgoW :: HMTm -> SFTm
 ------- TODO WARNING
 -- DO Something more here, this isn't the last step!
+
+runAlgoW :: HMTm -> SFTm
 runAlgoW t = let (sf,ty,tysbst) = algoW_Syn_HMTm (wrap_HMTm (sem_HMTm t) inh_HMTm)
-             in sf
+                 tyscheme       = gen (applySubst tysbst [("x", PlainTy (Alpha "Bool"))]) ty
+                 finaltype      = turnIntoTypedTerm sf tyscheme
+             in  finaltype
+
+turnIntoTypedTerm :: SFTm -> TyScheme -> SFTm
+turnIntoTypedTerm tm (PlainTy t)    = SFTyApp tm (convertType t)
+turnIntoTypedTerm tm (Forall tv ts) = undefined
+
+convertType :: Ty -> SFTy
+convertType (Alpha t) = SFTyVar t
+convertType _ = undefined
+
 -- | The top-level inherited attribute to be passed to an attribute grammar
 -- for System F. In our case, we want to start with an empty type 
 -- environment.
 inh_HMTm :: Inh_HMTm
-inh_HMTm = Inh_HMTm { typeEnvironment_Inh_HMTm = []
+inh_HMTm = Inh_HMTm { typeEnvironment_Inh_HMTm = [("x", PlainTy (Alpha "Bool"))]
                     , counter_Inh_HMTm = 0
                     }
