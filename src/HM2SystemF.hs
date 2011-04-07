289f6e157a1bfb532ce8cefd41542e9269bfc6af
Paul van der Walt <paul@denknerd.org>
2011-04-07 17:11:26 +0100

    place to call the AG functions

---
+++ src/HM2SystemF.hs
@@ -0,0 +1,17 @@
+import CCO.Component (Component, component, printer, ioWrap)
+import CCO.HM.Base (Tm)
+import qualified CCO.SystemF.Base as SF (Tm (Var))
+import CCO.Tree (ATerm, toTree, parser, fromTree)
+import Control.Arrow (arr, (>>>))
+
+main = ioWrap $
+        parser >>>
+        (component toTree :: Component ATerm Tm) >>>
+        convertAndType >>>
+        (arr fromTree :: Component SF.Tm ATerm) >>>
+        printer
+
+-- | constructs a really simple term, for testing.
+convertAndType :: Component Tm SF.Tm
+convertAndType = component $ (\s -> do return (SF.Var "s"))
+
