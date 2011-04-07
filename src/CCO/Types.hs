6b0d6569bcc13d147de59eec87de66242b50462b
Paul van der Walt <paul@denknerd.org>
2011-04-07 18:04:48 +0100

    start of unify

--- src/CCO/Types.hs
+++ src/CCO/Types.hs
@@ -33,3 +33,22 @@ module CCO.Types where
     instance Substitutable TyEnv where
         applySubst s [] = []
         applySubst s ((v,ts):r) = (v,applySubst s ts):applySubst s r
+
+    unify :: Ty -> Ty -> TySubst
+    unify t1@(TyVar tv1) t2@(TyVar tv2) | tv1 == tv2 = Identity
+                                        | not (elem tv1 (ftvT t2)) = Sub tv1 t2
+                                        | not (elem tv2 (ftvT t1)) = Sub tv2 t1
+                                        | otherwise = trace "unify" undefined
+    unify (TyVar tv1) t | not (elem tv1 (ftvT t)) = Sub tv1 t
+                        | otherwise = error$"occurs check: " ++
+                                                          show tv1 ++ ", " ++
+                                                          show t
+    unify t (TyVar tv2) | not (elem tv2 (ftvT t)) = Sub tv2 t
+                        | otherwise = error "occurs check2"
+    unify (Arr t11 t12) (Arr t21 t22) = let theta1 = unify t11 t21
+                                            theta2 = unify
+                                                        (applySubst theta1 t12)
+                                                        (applySubst theta1 t22)
+                                        in Dot theta2 theta1
+        
+    ftvT = undefined
